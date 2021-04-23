b5fe209c8802833bf3172bf95c93483183509eb5
Noureldin Kamel <noureldinashraf6@gmail.com>
2021-04-23 21:47:33 +0300

    adds admin role management

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -37,6 +37,14 @@ namespace TestAPI.Controllers
             // return Ok(new { error = "access denied" });
         }
 
+        [HttpGet("userdata")]
+        [Authorize]
+        public async Task<ActionResult> GetData()
+        {
+            var response = await Helper.Sendrequest("/userdata", Method.GET, Helper.GetTokenFromRequest(this.Request));
+            return Ok(new { response = response.Content });
+        }
+
         [HttpGet("loggedout")]
         public async Task<ActionResult> GetNotSignedin()
         {
@@ -56,6 +64,26 @@ namespace TestAPI.Controllers
         {
             return Ok(new { response = "testapi: success" });
         }
+
+        [HttpGet("addadmin")]
+        [Authorize]
+        public async Task<ActionResult> AddAdmin()
+        {
+            var response = await Helper.Sendrequest("/role/Admin", Method.POST, Helper.GetTokenFromRequest(this.Request));
+            System.Console.WriteLine("response: " + response.Content);
+            return Ok(new { response = "testapi: success" });
+        }
+
+        [HttpGet("removeadmin")]
+        [Authorize]
+        public async Task<ActionResult> removeadmin()
+        {
+            var response = await Helper.Sendrequest("/role/Admin", Method.DELETE, Helper.GetTokenFromRequest(this.Request));
+            System.Console.WriteLine("response: " + response.Content);
+            return Ok(new { response = "testapi: success" });
+        }
+
+
 
     }
 }
